345e47f785c5b640cb7fbcb0a0e9d2ec5e109c08
Goutam Tadi <gtadi@pivotal.io>
2017-10-13 13:42:45 -0700

    Print gcc and g++ versions as part configure
    - Extract common code into methods
     -- exporting LD_LIBRARY_PATH in greenplum_path
     -- creating demo cluster
    
    Signed-off-by: Larry Hamel <lhamel@pivotal.io>

--- concourse/scripts/builds/GpBuild.py
+++ concourse/scripts/builds/GpBuild.py
@@ -6,6 +6,8 @@ class GpBuild(GpdbBuildBase):
         self.mode = 'on' if mode == 'orca' else 'off'
 
     def configure(self):
+        subprocess.call(["g++","--version"])
+        subprocess.call(["gcc","--version"])
         return subprocess.call(["./configure",
                                 "--enable-mapreduce",
                                 "--with-gssapi",
@@ -16,14 +18,10 @@ class GpBuild(GpdbBuildBase):
                                 "--prefix=/usr/local/gpdb"], cwd="gpdb_src")
 
     def icg(self):
-        status = subprocess.call(
-            "printf '\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib\nexport \
-            LD_LIBRARY_PATH' >> /usr/local/gpdb/greenplum_path.sh", shell=True)
+        status = self.export_Ld_Library_Path_to_Greenplum_Path()
         if status:
             return status
-        status = subprocess.call([
-            "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
-            && make create-demo-cluster DEFAULT_QD_MAX_CONNECT=150\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
+        status = self.create_Demo_Cluster()
         if status:
             return status
         return subprocess.call([
@@ -32,17 +30,23 @@ class GpBuild(GpdbBuildBase):
             make -C src/test installcheck-good\"".format(self.mode)], cwd="gpdb_src", shell=True)
 
     def icw(self):
-        status = subprocess.call(
-            "printf '\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib\nexport \
-            LD_LIBRARY_PATH' >> /usr/local/gpdb/greenplum_path.sh", shell=True)
+        status = self.export_Ld_Library_Path_to_Greenplum_Path()
         if status:
             return status
-        status = subprocess.call([
-            "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
-            && make create-demo-cluster DEFAULT_QD_MAX_CONNECT=150\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
+        status = self.create_Demo_Cluster()
         if status:
             return status
         return subprocess.call([
             "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
             && source gpAux/gpdemo/gpdemo-env.sh && PGOPTIONS='-c optimizer={0}' \
             make installcheck-world\"".format(self.mode)], cwd="gpdb_src", shell=True)
+
+    def create_Demo_Cluster(self):
+        return subprocess.call([
+            "runuser gpadmin -c \"source /usr/local/gpdb/greenplum_path.sh \
+            && make create-demo-cluster DEFAULT_QD_MAX_CONNECT=150\""], cwd="gpdb_src/gpAux/gpdemo", shell=True)
+
+    def export_Ld_Library_Path_to_Greenplum_Path(self):
+        return subprocess.call(
+            "printf '\nLD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib\nexport \
+            LD_LIBRARY_PATH' >> /usr/local/gpdb/greenplum_path.sh", shell=True)
